8891ccc11274a94513e7d7664f02143500605562
lkcf0 <lkcf@tutanota.com>
2022-06-27 17:47:16 -0500

    added functions to add subtract divide and multiply

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(a, b) {
+   let sum = a + b;
+   return sum
+};
+
+
+function subtract(a, b) {
+    let sum = a - b;
+    return sum
+};
+
+
+function multiply(a, b) {
+    let sum = a * b;
+    return sum
+};
+ 
+
+function divide(a, b) {
+    let sum = a /= b;
+    return sum
+};
+
+
+function operate() {
+
+
+};
