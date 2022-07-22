26e68775e0b7978e1eba4c7170e22e0ba9aae043
lkcf <lkcf@tutanota.com>
2022-07-21 19:58:10 -0500

    + selector for html operator and num and other button

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+class Calculator {
+    constructor(prevTxt, currentTxt) {
+        this.prevTxt = prevTxt
+        this.currentTxt = currentTxt
+    }
+
+    clear() {
+
+    }
+
+    delete() {
+
+    }
+
+    appendNum(number) {
+
+    }
+
+    pickOperator(operation)
+
+    operate() {
+
+    }
+
+    display() {
+        
+    }
+}
+
+const numberBut = document.querySelectorAll('[data-num]')
+const operatorBut = document.querySelectorAll('[data-operator]')
+const clearBut = document.querySelector('[data-clear]')
+const delBut = document.querySelector('[data-del]')
+const equalBut = document.querySelector('[data-equal]')
+const prevTxt = document.querySelector('[data-prev]')
+const currentTxt = document.querySelector('[data-current]')
+
+
 function add(a, b) {
    let sum = a + b;
    return sum
@@ -21,8 +59,26 @@ function divide(a, b) {
     return sum
 };
 
+const display = "";
 
-function operate() {
+// passes the appropirate operator function when called //
+function operate(operator, a, b) {
+    switch(operator){
+            case '+':
+                return add(a, b);
+                break;
 
+            case '-':
+                return subtract(a, b);
+                break;
 
+            case '*':
+                return multiply(a, b);
+                break;
+
+            case '/':
+                return divide(a, b);
+                break;
+     }
 };
+
