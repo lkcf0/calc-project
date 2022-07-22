f9b8e422149ebae0aebaca203568e45ae81e929e
lkcf <lkcf@tutanota.com>
2022-07-21 22:01:07 -0500

    fixed typos connected html buttons to show with javascript display on page

--- script.js
+++ script.js
@@ -1,84 +1,99 @@
+
 class Calculator {
-    constructor(prevTxt, currentTxt) {
-        this.prevTxt = prevTxt
-        this.currentTxt = currentTxt
+    constructor(previousNumTxt, currentNumTxt) {
+        this.previousNumTxt = previousNumTxt
+        this.currentNumTxt = currentNumTxt
+        this.clear()
     }
 
     clear() {
-
+        this.currentNum = ''
+        this.previousNum = ''
+        this.operation = undefined
     }
 
     delete() {
 
     }
 
-    appendNum(number) {
+    appendNumber(number) {
+        this.currentNum = number
+    }
+
+    chooseOperation(operation) {
 
     }
 
-    pickOperator(operation)
-
-    operate() {
+    compute() {
 
     }
 
-    display() {
-        
+    updateDisplay() {
+        this.currentNumTxt.innerText = this.currentNum
     }
 }
 
-const numberBut = document.querySelectorAll('[data-num]')
-const operatorBut = document.querySelectorAll('[data-operator]')
-const clearBut = document.querySelector('[data-clear]')
-const delBut = document.querySelector('[data-del]')
-const equalBut = document.querySelector('[data-equal]')
-const prevTxt = document.querySelector('[data-prev]')
-const currentTxt = document.querySelector('[data-current]')
+const numberButtons = document.querySelectorAll('[data-num]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const allClearButton = document.querySelector('[data-clear]')
+const deleteButton = document.querySelector('[data-del]')
+const equalsButton = document.querySelector('[data-equals]')
+const previousNumTxt = document.querySelector('[data-previous-num]')
+const currentNumTxt = document.querySelector('[data-current-num]')
+
+const calculator = new Calculator(previousNumTxt, currentNumTxt)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
 
 
-function add(a, b) {
-   let sum = a + b;
-   return sum
-};
+// function add(a, b) {
+//    let sum = a + b;
+//    return sum
+// };
 
 
-function subtract(a, b) {
-    let sum = a - b;
-    return sum
-};
+// function subtract(a, b) {
+//     let sum = a - b;
+//     return sum
+// };
 
 
-function multiply(a, b) {
-    let sum = a * b;
-    return sum
-};
+// function multiply(a, b) {
+//     let sum = a * b;
+//     return sum
+// };
  
 
-function divide(a, b) {
-    let sum = a /= b;
-    return sum
-};
+// function divide(a, b) {
+//     let sum = a /= b;
+//     return sum
+// };
 
-const display = "";
+// const display = "";
 
-// passes the appropirate operator function when called //
-function operate(operator, a, b) {
-    switch(operator){
-            case '+':
-                return add(a, b);
-                break;
+// // passes the appropirate operator function when called //
+// function operate(operator, a, b) {
+//     switch(operator){
+//             case '+':
+//                 return add(a, b);
+//                 break;
 
-            case '-':
-                return subtract(a, b);
-                break;
+//             case '-':
+//                 return subtract(a, b);
+//                 break;
 
-            case '*':
-                return multiply(a, b);
-                break;
+//             case '*':
+//                 return multiply(a, b);
+//                 break;
 
-            case '/':
-                return divide(a, b);
-                break;
-     }
-};
+//             case '/':
+//                 return divide(a, b);
+//                 break;
+//      }
+// };
 
